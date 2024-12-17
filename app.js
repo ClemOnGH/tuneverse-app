0bbc6f97babd2634b17730d7b3083e60f60c4ee8
ClemOnGH <sora.never@gmail.com>
2024-12-17 16:28:28 +0100

    Update 0.0.1

---
+++ app.js
@@ -0,0 +1,8 @@
+// Last.FM API : https://www.last.fm/api/intro
+// Spotify API : https://developer.spotify.com/documentation/web-api
+// Koel API : https://docs.koel.dev/guide/what-is-koel#installation
+
+const spotify = {
+    clientID: "20501cd4492c46d8b1e87c3b27d4822d",
+    clientSecret: "54b3e851104342368449b310c9a66e37",
+};
