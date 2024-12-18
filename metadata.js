e2926f4be8c86c071722cb6661c74403eb1d82d1
Julien <julien.bossaert17@gmail.com>
2024-12-18 16:19:08 +0100

    "l"

---
+++ metadata.js
@@ -0,0 +1,6 @@
+let devicesButton = document.getElementById("devices-button")
+let metaData = document.getElementById("metadata")
+
+devicesButton.addEventListener("click", () => {
+    metaData.innerHTML += "efz"
+})
